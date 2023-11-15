a5bfdb04063f8402599ee13a8560c57ba9c305bd
Chiungue123 <stagg.roberto@gmail.com>
2023-11-14 16:33:39 -0800

    Created Packages for Entities, Controller, Service & Repository

--- BackEnd/src/main/java/com/booktracker/backend/controller/BookController.java
+++ BackEnd/src/main/java/com/booktracker/backend/controller/BookController.java
@@ -1,5 +1,20 @@
 package com.booktracker.backend.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class BookController {
 
+	final Logger logger = LoggerFactory.getLogger(this.getClass());
+	
+	@GetMapping("/")
+	public String hello() {
+		
+		logger.debug("GET Hello");
+		return "Hello";
+	}
+	
 }
--- BackEnd/src/main/java/com/booktracker/backend/jpa/BookJPA.java
+++ BackEnd/src/main/java/com/booktracker/backend/jpa/BookJPA.java
@@ -1,10 +1,81 @@
 package com.booktracker.backend.jpa;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name="BOOK")
 public class BookJPA {
-
 	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="ID_BOOK")
+	private int id;
+	
+	@Column(name="TITLE")
+	private String title;
+	
+	@Column(name="AUTHOR")
+	private String author;
+	
+	@Column(name="PUBLISHED_YEAR")
+	private Integer publishedYear;
+
+	public BookJPA() {
+		
+	}
+	
+	public BookJPA(int id, String title, String author, Integer publishedYear) {
+		super();
+		this.id = id;
+		this.title = title;
+		this.author = author;
+		this.publishedYear = publishedYear;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public Integer getPublishedYear() {
+		return publishedYear;
+	}
+
+	public void setPublishedYear(Integer publishedYear) {
+		this.publishedYear = publishedYear;
+	}
+
+	@Override
+	public String toString() {
+		return "BookJPA [id=" + id + 
+				", title=" + title + 
+				", author=" + author + 
+				", publishedYear=" + publishedYear +
+				"]";
+	}
 	
 }
