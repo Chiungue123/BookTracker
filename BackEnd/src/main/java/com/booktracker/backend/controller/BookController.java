a263022d1981ab7705e8aff1e91a2b5c75b2a1e2
Chiungue123 <stagg.roberto@gmail.com>
2023-11-16 18:29:52 -0800

    Add routes to BookController and enable CORS support
    - Implemented new routes in BookController to manage book data.
    - Added @CrossOrigin annotation to enable CORS, allowing requests from the Angular frontend at http://localhost:4200.
    - Created test data with three BookJPA instances for initial testing and validation of routes.
    - Included logging for each route to facilitate debugging and monitoring.

--- BackEnd/src/main/java/com/booktracker/backend/controller/BookController.java
+++ BackEnd/src/main/java/com/booktracker/backend/controller/BookController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.booktracker.backend.jpa.BookJPA;
 import com.booktracker.backend.service.BookService;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 public class BookController {
 	
